fbaa307e229d328d043298d9016ac7b83dadbb7d
chiragdeep01 <cdeep.verma@gmail.com>
2023-02-24 19:34:41 +0530

    added messages table

--- room/models.py
+++ room/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Room(models.Model):
     name = models.CharField(max_length = 100)
     slug = models.SlugField(unique=True)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, related_name='message', on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name='message', on_delete=models.CASCADE)
+    message = models.TextField()
+    date_added = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('date_added',)
