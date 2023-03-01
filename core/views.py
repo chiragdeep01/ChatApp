0c647ed77a532c47485dfa1f07e7e2d1abf74914
chiragdeep01 <cdeep.verma@gmail.com>
2023-03-01 12:16:19 +0530

    heroku config added

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import SignUpForm
 from django.contrib.auth import login
-
+from room.models import Room_Member,Room
 def frontpage(request):
     return render(request,"core/frontpage.html")
 
@@ -11,6 +11,8 @@ def signupPage(request):
         if form.is_valid():
             user = form.save()
             login(request,user)
+            room = Room.objects.get(slug = 'welcome')
+            Room_Member.objects.create(user = user, room = room)
             return redirect('frontpage')
     else:
         form = SignUpForm()
