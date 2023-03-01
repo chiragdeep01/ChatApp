e38ec50e92dec73e7498e4b0421f9d78eacf5e86
chiragdeep01 <cdeep.verma@gmail.com>
2023-03-01 12:48:05 +0530

    railway config done

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-sm)&digzgvwgqrc&-&rf4qgx*hi1$sm*a=&or)jl_c%(%c4#44
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIRS = [
-    BASE_DIR,"static"
+    os.path.join(BASE_DIR,"static"),
 ]
-MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- chatapp/urls.py
+++ chatapp/urls.py
@@ -17,15 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 import room
 
-from django.views.static import serve
-from django.conf.urls import url
-from django.conf import settings
 
 urlpatterns = [
     path('',include('core.urls')),
     path('rooms/',include('room.urls')),
     path("admin/", admin.site.urls),
     path('CreateRoom/',room.views.room_create,name='room_create'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
